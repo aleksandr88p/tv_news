069054d1eb643779ac112011b78276e6cd9d9f6e
aleksandr88p <aleksandr88p@gmail.com>
2023-05-30 22:07:22 +0200

    similar articles

---
+++ for_analisis_and_gpt/tf_idf_two.py
@@ -0,0 +1,97 @@
+import json
+
+import mysql.connector
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import datetime
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def connect_to_database(host, user, password, database):
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    return conn
+
+def fetch_articles_from_tables(conn, table_names, days_ago):
+    dataframes = []
+    some_days_ago = (datetime.datetime.now() - datetime.timedelta(days=days_ago)).strftime('%Y-%m-%d %H:%M:%S')
+    for table in table_names:
+        sql_query = f"SELECT * FROM {table} WHERE parsing_date >= '{some_days_ago}'"
+        df = pd.read_sql(sql_query, con=conn)
+        df["source"] = table
+        dataframes.append(df)
+    return dataframes
+
+def preprocess_articles(articles_df):
+    stop_words = set(stopwords.words('english'))
+    articles_df['article_text'] = articles_df['article_text'].apply(lambda x: ' '.join(
+        [word for word in word_tokenize(x) if word.casefold() not in stop_words]
+    ))
+    vectorizer = TfidfVectorizer()
+    article_vectors = vectorizer.fit_transform(articles_df['article_text'])
+    return article_vectors
+
+def find_similar_articles(article_vectors, threshold):
+    similarities = cosine_similarity(article_vectors)
+    similar_articles_dict = {}
+    for i in range(len(similarities)):
+        for j in range(i + 1, len(similarities)):
+            if similarities[i, j] > threshold:
+                if all_articles.iloc[i]["url"] not in similar_articles_dict:
+                    similar_articles_dict[all_articles.iloc[i]["url"]] = []
+                similar_articles_dict[all_articles.iloc[i]["url"]].append(all_articles.iloc[j]["url"])
+    return similar_articles_dict
+
+def print_article_groups(similar_articles_dict, all_articles):
+    printed_groups = set()
+    c = 0
+    for key, val in similar_articles_dict.items():
+        group = set(val + [key])
+        sources = [all_articles.loc[all_articles['url'] == article_url]['source'].values[0] for article_url in group]
+        if len(set(sources)) < 3:
+            continue
+        if str(group) not in printed_groups:
+            c += 1
+            print(f"Articles group {c}:")
+            for article_url in group:
+                article = all_articles.loc[all_articles['url'] == article_url]
+                print(f"URL: {article_url}")
+            printed_groups.add(str(group))
+
+def create_article_groups_dict(similar_articles_dict, all_articles):
+    article_groups_dict = {}
+    c = 0
+    for key, val in similar_articles_dict.items():
+        group = set(val + [key])
+        sources = [all_articles.loc[all_articles['url'] == article_url]['source'].values[0] for article_url in group]
+        if len(set(sources)) < 3:
+            continue
+        c += 1
+        group_key = f"Articles group {c}"
+        article_groups_dict[group_key] = list(group)
+    return article_groups_dict
+
+
+conn = connect_to_database("none", "none", "none", "none")
+
+table_names = ['deadline_film', 'deadline_tv', 'hollywoodreporter_movies_news',
+               'hollywoodreporter_tv_news', 'indiwire_film', 'indiwire_tv',
+               'variety_film_news', 'variety_tv_news']
+
+dataframes = fetch_articles_from_tables(conn, table_names, 2)
+all_articles = pd.concat(dataframes, ignore_index=True)
+
+article_vectors = preprocess_articles(all_articles)
+similar_articles_dict = find_similar_articles(article_vectors, 0.5)
+d = create_article_groups_dict(similar_articles_dict, all_articles)
+
+print(json.dumps(d, indent=4))
--- main.py
+++ main.py
@@ -1,35 +1,50 @@
+import schedule
+import time
+
 from deadline import find_links_deadline, find_article_deadline
 from hollywoodreporter import find_links_hollywoodreporter, find_article_hollywoodreporter
 from indiwire import find_links_indiwire, find_article_indiwire
 from variety import find_links_variety, find_article_variety
 from processing import process_links
 
-# Define the sources
-sources = [
-    {
-        "name": "Deadline",
-        "find_links": find_links_deadline,
-        "find_article": find_article_deadline,
-    },
-    {
-        "name": "Hollywood Reporter",
-        "find_links": find_links_hollywoodreporter,
-        "find_article": find_article_hollywoodreporter,
-    },
-    {
-        "name": "Indiwire",
-        "find_links": find_links_indiwire,
-        "find_article": find_article_indiwire,
-    },
-    {
-        "name": "Variety",
-        "find_links": find_links_variety,
-        "find_article": find_article_variety,
-    },
-]
 
-# Process articles from all sources
-for source in sources:
-    print(f"Processing articles from {source['name']}...")
-    links = source['find_links']()
-    process_links(source['find_article'], links)
+def process_articles():
+    sources = [
+        {
+            "name": "Deadline",
+            "find_links": find_links_deadline,
+            "find_article": find_article_deadline,
+        },
+        {
+            "name": "Hollywood Reporter",
+            "find_links": find_links_hollywoodreporter,
+            "find_article": find_article_hollywoodreporter,
+        },
+        {
+            "name": "Indiwire",
+            "find_links": find_links_indiwire,
+            "find_article": find_article_indiwire,
+        },
+        {
+            "name": "Variety",
+            "find_links": find_links_variety,
+            "find_article": find_article_variety,
+        },
+    ]
+
+    for source in sources:
+        print(f"Processing articles from {source['name']}...")
+        links = source['find_links']()
+        process_links(source['find_article'], links)
+
+
+def run_schedule():
+    schedule.every(5).hours.do(process_articles)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run_schedule()
